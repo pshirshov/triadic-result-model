6904097d9beccbcbe75db08b0fccae57211bd58d
pshirshov <pshirshov@gmail.com>
2018-08-09 23:35:44 +0100

    cleanups

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -4,33 +4,37 @@ import scala.concurrent.duration.Duration
 import scala.language.{higherKinds, implicitConversions}
 
 trait WithResult {
-  type RAlt[E, R]
-  type RUni[R]
+  type Or[E, R]
+  type Just[R]
 }
 
-object XIO {
-  type XRAlt[E, V] = IO[Either[Throwable, E], V]
-  type XRUni[V] = IO[Left[Throwable, Nothing], V]
+trait ZioResult extends WithResult {
+  type Or[E, V] = IO[Either[Throwable, E], V]
+  type Just[V] = IO[Left[Throwable, Nothing], V]
 
-  implicit def convert[L, R](io: IO[Throwable, Either[L, R]]): XRAlt[L, R] = {
-    io.redeem[Either[Throwable, L], R](t => IO.fail(Left(t)), {
-      case Left(v) =>
-        IO.fail(Right(v))
-      case Right(v) =>
-        IO.point(v)
-    })
+  implicit class ZioEitherExt[L, R](val io: IO[Throwable, Either[L, R]]) {
+    def trifunctor: Or[L, R] = {
+      io.redeem[Either[Throwable, L], R](t => IO.fail(Left(t)), {
+        case Left(v) =>
+          IO.fail(Right(v))
+        case Right(v) =>
+          IO.point(v)
+      })
+    }
   }
 
-  implicit def convert1[R](io: IO[Throwable, R]): XRUni[R] = {
-    io.redeem[Left[Throwable, Nothing], R](t => IO.fail(Left(t)), v => IO.point(v))
+  implicit class ZioExt[R](val io: IO[Throwable, R]) {
+    implicit def trifunctor: Just[R] = {
+      io.redeem[Left[Throwable, Nothing], R](t => IO.fail(Left(t)), v => IO.point(v))
+    }
   }
 
-  def apply[L, R](v: => Either[L, R]): XRAlt[L, R] = {
-    IO.syncThrowable(v)
+  def choice[L, R](v: => Either[L, R]): Or[L, R] = {
+    IO.syncThrowable(v).trifunctor
   }
 
-  def unary[R](v: => R): XRUni[R] = {
-    IO.syncThrowable(v)
+  def just[R](v: => R): Just[R] = {
+    IO.syncThrowable(v).trifunctor
   }
 }
 
@@ -41,37 +45,33 @@ trait Service extends WithResult {
     case class SomeError() extends AnError
   }
 
-  def unary1(): RUni[Long]
-  def unary2(): RUni[Long]
+  def unary1(): Just[Long]
+  def unary2(): Just[Long]
 
-  def alternative1(): RAlt[AnError, Long]
-  def alternative2(): RAlt[AnError, Long]
-  def alternative3(): RAlt[AnError, Long]
+  def alternative1(): Or[AnError, Long]
+  def alternative2(): Or[AnError, Long]
+  def alternative3(): Or[AnError, Long]
 
 }
 
-class ServiceImpl extends Service {
-  import XIO._
-  type RAlt[E, V] = XRAlt[E, V]
-  type RUni[V] = XRUni[V]
-
-  override def unary1(): RUni[Long] = XIO.unary {
+class ServiceImpl extends Service with ZioResult {
+  override def unary1(): Just[Long] = just {
     99
   }
 
-  override def unary2(): RUni[Long] = XIO.unary {
+  override def unary2(): Just[Long] = just {
     ???
   }
 
-  override def alternative1(): RAlt[AnError, Long] = XIO {
+  override def alternative1(): Or[AnError, Long] = choice {
     Right(0L)
   }
 
-  override def alternative2(): RAlt[AnError, Long] = XIO {
+  override def alternative2(): Or[AnError, Long] = choice {
     Left(AnError.SomeError())
   }
 
-  override def alternative3(): RAlt[AnError, Long] = XIO {
+  override def alternative3(): Or[AnError, Long] = choice {
     ???
   }
 }
