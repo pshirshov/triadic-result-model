fdf1864418c8c0e279bcd66ac0d3aa9e5f57b3d4
Kaidax <kaidaxofficial@gmail.com>
2018-08-10 23:42:33 +0100

    convenient redeem3, attempt3, catchTerminated

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -18,40 +18,101 @@ trait ZioResult extends WithResult {
         case Left(v) =>
           IO.fail(Right(v))
         case Right(v) =>
-          IO.point(v)
+          IO.now(v)
       })
     }
   }
 
+  sealed trait Case[+E]
+  final case class CaseError[E](error: E) extends Case[E]
+  final case class CaseTerminated[E](error: Throwable) extends Case[E]
+
   implicit class ZioRecover[E, A](val io: IO[E, A]) {
-    def recoverTerminated(recoverWith: Throwable => A): IO[E, A] =
-      io.run.flatMap {
-        case ExitResult.Terminated(e :: _) =>
-          IO.point(recoverWith(e))
-        case ExitResult.Failed(error, defects) =>
-          throw new RuntimeException("dunno") // FIXME
-        case ExitResult.Completed(value) =>
-          IO.point[A](value)
-      }
 
-    def recoverAll(recoverWith: Case => A): IO[E, A] =
-      io.run.flatMap {
-        case ExitResult.Terminated(e :: _) =>
-          IO.point(recoverWith(CaseTerminate(e)))
+    /**
+      * IO.sync { 5 / 0 }.redeem3 (
+      *     { _: MyDomainException => IO.now(500) }
+      *   , { case _: ArithmeticException => IO.now(1000) }
+      *   , IO.now(_ * 2)
+      * )
+      */
+    def redeem3[E2, B](err: E => IO[E2, B], term: Throwable => IO[E2, B], succ: A => IO[E2, B]): IO[E2, B] =
+      io.run[E, A].flatMap {
+        case ExitResult.Completed(value) =>
+          succ(value)
         case ExitResult.Failed(error, _) =>
-          IO.point(recoverWith(CaseError(error)))
-        case ExitResult.Completed(value) =>
-          IO.point[A](value)
+          err(error)
+        case ExitResult.Terminated(e :: _) =>
+          term(e)
       }
 
-    sealed trait Case
-    final case class CaseError[EE](error: EE) extends Case
-    final case class CaseTerminate(error: Throwable) extends Case
+    /**
+      * IO.sync { 5 / 0 }.redeem3 (
+      *     { _: MyDomainException => 500 }
+      *   , { case _: ArithmeticException => 1000 }
+      *   , _ * 2
+      * )
+      */
+    def redeem3Pure[E2, B](err: E => B, term: Throwable => B, succ: A => B): IO[E2, B] =
+      redeem3(err.andThen(IO.now), term.andThen(IO.now), succ.andThen(IO.now))
+
+    /**
+      * IO.sync { 5 / 0 }.attempt3.flatMap {
+      *   case Right(value) =>
+      *     IO.now(value * 2)
+      *   case Left(CaseError(_: MyDomainException)) =>
+      *     IO.now(500)
+      *   case Left(CaseTerminated(_: ArithmeticException)) =>
+      *     IO.now(1000)
+      * }
+      */
+    def attempt3: IO[Nothing, Either[Case[E], A]] =
+      redeem3Pure(e => Left(CaseError(e)), t => Left(CaseTerminated(t)), Right(_))
+
+    /**
+      * IO.sync { 5 / 0 }.catchTerminated {
+      *   case _: ArithmeticException =>
+      *     IO.now(1000)
+      * }
+      */
+    def catchTerminated(recoverWith: Throwable => IO[E, A]): IO[E, A] =
+      redeem3(IO.fail, recoverWith, IO.now)
+
+    /**
+      * IO.sync { 5 / 0 }.catchTerminated {
+      *   case _: ArithmeticException =>
+      *     1000
+      * }
+      */
+    def catchTerminatedPure(recoverWith: Throwable => A): IO[E, A] =
+      catchTerminated(recoverWith.andThen(IO.now))
+
+    /**
+      * IO.sync { 5 / 0 }.catchBoth {
+      *   case CaseError(_: MyDomainException) =>
+      *     IO.now(500)
+      *   case CaseTerminated(_: ArithmeticException) =>
+      *     IO.now(1000)
+      * }
+      */
+    def catchBoth[E2](recoverWith: Case[E] => IO[E2, A]): IO[E2, A] =
+      redeem3[E2, A](e => recoverWith(CaseError(e)), t => recoverWith(CaseTerminated(t)), IO.now)
+
+    /**
+      * IO.sync { 5 / 0 }.catchBothPure {
+      *   case CaseError(_: MyDomainException) =>
+      *     500
+      *   case CaseTerminated(_: ArithmeticException) =>
+      *     1000
+      * }
+      */
+    def catchBothPure(recoverWith: Case[E] => A): IO[E, A] =
+      catchBoth(recoverWith.andThen(IO.now))
   }
 
   implicit class ZioExt[R](val io: IO[Throwable, R]) {
     implicit def trifunctor: Just[R] = {
-      io.redeem[Left[Throwable, Nothing], R](t => IO.fail(Left(t)), v => IO.point(v))
+      io.redeem[Left[Throwable, Nothing], R](t => IO.fail(Left(t)), v => IO.now(v))
     }
   }
 
